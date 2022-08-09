53f729f6d70c515751c602a308eecf3d98174999
John Kim <dhkdhd123@gmail.com>
2022-08-09 19:44:23 +0900

    ADD image domain

--- john/next.config.js
+++ john/next.config.js
@@ -8,6 +8,7 @@ const nextConfig = {
       'images.unsplash.com',
       'i.picsum.photos',
       'ifh.cc/g',
+      's3.us-west-2.amazonaws.com',
     ],
     format: ['image/png', 'image/webp', 'image/jpeg', 'image/jpg']
   },
