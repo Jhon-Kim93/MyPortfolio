ed3b116342b6fe5f2361f9dc1ef1197e07e66f8d
John Kim <dhkdhd123@gmail.com>
2022-08-11 13:21:39 +0900

    FIX hotfix

--- john/components/header.js
+++ john/components/header.js
@@ -32,7 +32,7 @@ export default function Header() {
             </Link>
             {/* <button onClick={isOpen = false} tabIndex="-1" className={`display:${isOpen ? inline : none} fixed inset-0 h-full w-full bg-dark-600/50 cursor-default`}></button> */}
             <div onClick={dropdownToggle} className="relative z-10 hover:text-gray-900">Contact me</div>
-            <div id='dropdown' className={`display:none absolute right-0 py-2 w-48 mt-2 bg-white-300 dark:bg-slate-600 rounded-lg shadow-xl`}>
+            <div id='dropdown' className="display:none absolute right-0 py-2 w-48 mt-2 bg-white-300 dark:bg-slate-600 rounded-lg shadow-xl">
               <a href="https://open.kakao.com/o/sTrnONte" className="block pr-4 py-1 mr-5 hover:text-gray-900">kakao</a>
               <a
                 href="https://vast-blouse-d8e.notion.site/FE-Developer-9809ac8996e84d9e9e86626239c102e2"
