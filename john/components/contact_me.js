505a59f34c7114b230fc5822f2e3820bbea63fd7
John Kim <dhkdhd123@gmail.com>
2022-08-11 13:59:20 +0900

    FIX hotfix

--- john/components/contact_me.js
+++ john/components/contact_me.js
@@ -2,12 +2,12 @@ export default function ffContactMe () {
 
   return (
     <div>
-      <span className="hover:text-gray-900"> ContactMe : </span>
-      <a href="https://open.kakao.com/o/sTrnONte" className="block mr-5 hover:text-gray-900">kakao | </a>
-      <a href="https://www.instagram.com/john_kim_9312/" className="block mr-5 hover:text-gray-900">instagram | </a>
+      <span className="hover:text-gray-900 ml-3"> ContactMe : </span>
+      <a href="https://open.kakao.com/o/sTrnONte" className="inline mr-5 hover:text-gray-900">kakao | </a>
+      <a href="https://www.instagram.com/john_kim_9312/" className="inline mr-5 hover:text-gray-900">instagram | </a>
       <a
         href="https://vast-blouse-d8e.notion.site/FE-Developer-9809ac8996e84d9e9e86626239c102e2"
-        className="block mr-5 hover:text-gray-900"
+        className="inline mr-5 hover:text-gray-900"
       >notion</a>
     </div>
   )
