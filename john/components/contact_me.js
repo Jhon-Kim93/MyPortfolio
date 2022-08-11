25c267b1bedcfd12f73783921057dd56f6f082a7
John Kim <dhkdhd123@gmail.com>
2022-08-11 13:51:06 +0900

    FIX hotfix

--- john/components/contact_me.js
+++ john/components/contact_me.js
@@ -8,7 +8,7 @@ export default function ContactMe () {
     <div className="relative">
       <button onClick={dropdownToggle()} className="relative z-10 hover:text-gray-900">Contact me</button>
       {/* <button onClick={isOpen = false} tabIndex="-1" className={`${isOpen ? "block" : "hidden"} fixed inset-0 h-full w-full bg-dark-600/50 cursor-default`}></button> */}
-      <div id='dropdown' className={`${isOpen ? "block" : "hidden"} absolute right-0 py-2 w-48 mt-2 bg-white-300 dark:bg-slate-600 rounded-lg shadow-xl`}>
+      <div id='dropdown' className="block absolute right-0 py-2 w-48 mt-2 bg-white-300 dark:bg-slate-600 rounded-lg shadow-xl">
         <a href="https://open.kakao.com/o/sTrnONte" className="block pr-4 py-1 mr-5 hover:text-gray-900">kakao</a>
         <a
           href="https://vast-blouse-d8e.notion.site/FE-Developer-9809ac8996e84d9e9e86626239c102e2"
