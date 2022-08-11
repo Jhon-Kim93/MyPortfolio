69d7e7dbde1e32302df7457ac274c02dd22c59a7
John Kim <dhkdhd123@gmail.com>
2022-08-11 13:16:49 +0900

    FIX hotfix

--- john/components/header.js
+++ john/components/header.js
@@ -30,16 +30,16 @@ export default function Header() {
             <Link href="/projects">
               <a className="mr-5 hover:text-gray-900">projects</a>
             </Link>
-            <button onClick={isOpen = false} tabIndex="-1" className={`display:${isOpen ? "inline" : "none"} fixed inset-0 h-full w-full bg-dark-600/50 cursor-default`}></button>
+            <button onClick={isOpen = false} tabIndex="-1" className={`display:${isOpen ? inline : none} fixed inset-0 h-full w-full bg-dark-600/50 cursor-default`}></button>
             <div onClick={dropdownToggle} className="relative z-10 hover:text-gray-900">Contact me</div>
-            <div id='dropdown' className={`display:${isOpen ? "inline" : "none"} absolute right-0 py-2 w-48 mt-2 bg-white-300 dark:bg-slate-600 rounded-lg shadow-xl`}>
-              <a href="https://open.kakao.com/o/sTrnONte" className="block px-4 py-1 mr-5 hover:text-gray-900">kakao | </a>
+            <div id='dropdown' className={`display:${isOpen ? inline : none} absolute right-0 py-2 w-48 mt-2 bg-white-300 dark:bg-slate-600 rounded-lg shadow-xl`}>
+              <a href="https://open.kakao.com/o/sTrnONte" className="block pr-4 py-1 mr-5 hover:text-gray-900">kakao</a>
               <a
                 href="https://vast-blouse-d8e.notion.site/FE-Developer-9809ac8996e84d9e9e86626239c102e2"
-                className="block px-4 py-1 mr-5 hover:text-gray-900"
-              >notion | </a>
-              <a href="https://github.com/John-Kim93" className="block px-4 py-1 mr-5 hover:text-gray-900">github | </a>
-              <a href="https://www.instagram.com/john_kim_9312/" className="block px-4 py-1 mr-5 hover:text-gray-900">instagram</a>
+                className="block pr-4 py-1 mr-5 hover:text-gray-900"
+              >notion</a>
+              <a href="https://github.com/John-Kim93" className="block pr-4 py-1 mr-5 hover:text-gray-900">github</a>
+              <a href="https://www.instagram.com/john_kim_9312/" className="block pr-4 py-1 mr-5 hover:text-gray-900">instagram</a>
             </div>
           </nav>
           <DarkModeBtn/>
